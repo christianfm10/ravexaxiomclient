1b0970e724f5c525a470aaea1d14c16bbf5c75b4
christianfm10 <christian.flores@dekoding.pe>
2025-12-24 14:11:59 -0500

    feat(init): Add AxiomWebSocketClient to module exports

--- src/axiomclient/__init__.py
+++ src/axiomclient/__init__.py
@@ -1,8 +1,9 @@
 from axiomclient.client import AxiomClient
+from axiomclient.auth.auth_manager import AuthManager as AxiomAuth
+from axiomclient.websocket._client import AxiomWebSocketClient
 
 __version__ = "1.2.0"
-__all__ = ["AxiomClient", "AxiomAuth", "__version__"]
-
+__all__ = ["AxiomClient", "AxiomAuth", "AxiomWebSocketClient", "__version__"]
 
 # def main() -> None:
 #     print("Hello from axiomclient!")
